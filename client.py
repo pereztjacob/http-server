3a367c78521c66d114fb106a8df4718ad72897c7
pereztjacob <pereztjacob@gmail.com>
2018-02-02 12:54:09 -0800

    partially through 1 and 2

--- client.py
+++ client.py
@@ -1,20 +1,20 @@
 import sys
 import socket
 
+s = socket.socket()
+host = socket.gethostname() 
+
 def client(eom, message):
 
-    s = socket.socket()
-
-    host = socket.gethostname() 
-
+# ////////////////////  CONNECT  ////////////////////////////////////////////////////////
     try:
-        s.connect( ( host, 12345 ) )
+        s.connect((host, 12345))
     except:
         print("Connection Failed")
     else:
-        msg = str.encode(message) 
+        msg = str.encode(message) # TURN TO BINARY
 
-        s.sendall( msg )
+        s.sendall( msg ) # SEND TO SERVER
 
         if eom == "close":
             print(message)
@@ -23,22 +23,34 @@ def client(eom, message):
         elif eom == "LF":
             s.send(str.encode("\n"))
 
-        raw = s.recv(len(message))
+        raw = s.recv(len(message)) # RECIEVE FULL MESSAGE
 
         result = raw.decode()
+        print(result)
 
-        
         s.close()
 
-        if result == message:
-            print("OK: got echo")
-            return True
-        else:
-            print("FAILED: got some other message: " + result)
-            return False	
+# //////////////////////////  ASSEMBLING MESSAGE  /////////////////////////////////////
+def make_request(message):
+    REQUEST = "GET"
+    VERSION = "HTTP/1.1"
+    CRLF    = "/r/n"
+
+    request = REQUEST + " " + VERSION + CRLF
+    headers = "Host: {0}".format(host) + CRLF
+    head = request + headers
+    body = message + CRLF
+    full = head + CRLF + body + CRLF
+
+    client('close', full) # CALL CLIENT FOR (MESSAGE)
+
+def parse_response():
+    while get bytes
+        break on CRLF
 	
 if __name__ == "__main__":
-	if len(sys.argv) <= 2:
-		print("Usage: client EOM message")
-	else:
-		client(sys.argv[1], sys.argv[2])
+    if len(sys.argv) <= 2:
+        print("Usage: client EOM message")
+    else:
+        # client(sys.argv[1], sys.argv[2])
+        response_ok('hello world')
--- server.py
+++ server.py
@@ -2,27 +2,27 @@ import socket
 
 def server():
 	
+    # ////////////////////////////  BIND AND LISTEN  ////////////////////////////////////
     s = socket.socket()         
-
     host = socket.gethostname() 
-
     s.bind((host, 12345))
-
     s.listen(5)
 
     while True:
-        conn, addr = s.accept()
+        conn, addr = s.accept() # BEGIN CONNECTION
         
-        message = b''
-        while True:
-            data = conn.recv(1)
+        # message = b''
+        # while True:
+        #     data = conn.recv(1)
             
-            if data == b'':
-                break
-            if data ==  b'\n':
-                break
+        #     if data == b'':
+        #         break
+        #     if data ==  b'\n':
+        #         break
                 
-            message += data
+        #     message += data
+
+        message = parse_request(conn)
 
         print(message)
         conn.send(message)
@@ -31,5 +31,19 @@ def server():
 
         if message.decode() == 'q':
             break
+
+def parse_request(socket):
+    parse_head(socket)
+    parse_body(socket)
+
+def parse_head(socket):
+    while get bytes
+        break on CRLF
+    expect CRLF
+
+def parse_body(socket):
+    while get bytes:
+        break on CRLF
+    expect CRLF
 
 server()
